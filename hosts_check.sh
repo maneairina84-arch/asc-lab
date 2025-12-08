0d0e2d1c71831a3713aa88a6638e584fb452c789
Manea Irina <maneairina84@gmail.com>
2025-12-08 17:43:45 +0200

    script pt validraea IP

--- hosts_check.sh
+++ hosts_check.sh
@@ -1,6 +1,6 @@
 cat /etc/hosts | while read ip name; do
 [ -z "$name " ] && continue
-dns_ip=$(nslookup "$name " 2>/dev/null | awk 'Adress: /{print $2; exit}')
+dns_ip=$(nslookup "$name " 8.8.8.8  2>/dev/null | awk 'Adress: /{print $2; exit}')
 
 if [ -n "$dns_ip" ]; then
 	if  [ "$dns_ip"!= "$ip" ]; then
